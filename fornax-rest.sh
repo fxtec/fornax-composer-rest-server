47141b2a6fc91ce747b6ef93a551e05e0d6d8d8e
DaviSM <davimesquita@gmail.com>
2019-01-03 19:53:16 -0200

    com servico rest

--- fornax-rest.sh
+++ fornax-rest.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 
 et $FORNAX_CARD.card > /dev/null
 if [[ "$?" != "0" ]]
@@ -6,5 +7,5 @@ then
 else
     etoutput $FORNAX_CARD.card > /dev/null
     composer card import --file ./$FORNAX_CARD.card --card $FORNAX_CARD
-    pm2-docker composer-rest-server -c $FORNAX_CARD -n always -u true -d y -w true
+    composer-rest-server -c $FORNAX_CARD -n always -u true -d y -w true
 fi
